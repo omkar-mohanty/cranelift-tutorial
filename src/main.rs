557ab22899cde3f7fc1b8c3af89d4bca5a3205e4
omkar-mohanty <franzohouser@gmail.com>
2023-04-16 12:56:37 +0530

    main: finzalize functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use cranelift::{codegen::{self, ir::UserFuncName}, prelude::{FunctionBuilderContext, FunctionBuilder, InstBuilder}};
+use cranelift::{
+    codegen::{self, ir::UserFuncName, isa, Context},
+    prelude::{settings, FunctionBuilder, FunctionBuilderContext, InstBuilder},
+};
 
-use codegen::{ir::{types::I64, AbiParam, Function, Signature},
-isa::CallConv};
+use codegen::{
+    ir::{types::I64, AbiParam, Function, Signature},
+    isa::CallConv,
+};
+use target_lexicon::Triple;
 
 fn main() {
     let mut sig = Signature::new(CallConv::SystemV);
@@ -11,7 +17,7 @@ fn main() {
     let mut func = Function::with_name_signature(UserFuncName::default(), sig);
 
     let mut func_ctx = FunctionBuilderContext::new();
-    let mut builder = FunctionBuilder::new(&mut func,&mut func_ctx);
+    let mut builder = FunctionBuilder::new(&mut func, &mut func_ctx);
 
     let block = builder.create_block();
 
@@ -26,4 +32,34 @@ fn main() {
     builder.finalize();
 
     println!("func : {}", func.display());
+
+    let builder = settings::builder();
+
+    let flags = settings::Flags::new(builder);
+
+    let isa = match isa::lookup(Triple::host()) {
+        Err(err) => panic!("Error looking up target {}", err),
+        Ok(isa_builder) => isa_builder.finish(flags).unwrap(),
+    };
+
+    let mut ctx = Context::for_function(func);
+
+    let code = ctx.compile(&*isa).unwrap();
+
+    let len = code.code_buffer().len();
+
+    let mut buffer = memmap2::MmapOptions::new().len(len).map_anon().unwrap();
+
+    buffer.copy_from_slice(code.code_buffer());
+
+    let buffer = buffer.make_exec().unwrap();
+
+    let x = unsafe {
+        let code_fn: unsafe extern "sysv64" fn(usize) -> usize =
+            std::mem::transmute(buffer.as_ptr());
+
+        code_fn(1)
+    };
+
+    println!("out: {}", x);
 }
